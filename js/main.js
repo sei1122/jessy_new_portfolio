f6d3545af025e6035d7cc5c1322d9c81dcf694d5
seiko igi <seikoigi@seikos-MBP.attlocal.net>
2020-04-21 16:10:06 -0700

    add humbarger menu

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// navigation menu
+document.getElementById('sm-menu-f').addEventListener('click', checkNav);
+window.addEventListener("keyup", function(e) {
+  if (e.keyCode == 27) closeNav();
+}, false);
+
+function checkNav() {
+  if (document.body.classList.contains('hamburgler-active')) {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function closeNav() {
+  document.body.classList.remove('hamburgler-active');
+}
+
+function openNav() {
+  document.body.classList.add('hamburgler-active');
+}
