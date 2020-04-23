2cf2b5332654e7ba1354b45d522d24e549db1138
seiko igi <seikoigi@seikos-MBP.attlocal.net>
2020-04-23 15:30:01 -0700

    finish index page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // navigation menu
 document.getElementById('sm-menu-f').addEventListener('click', checkNav);
-window.addEventListener("keyup", function(e) {
+window.addEventListener("keyup", function (e) {
   if (e.keyCode == 27) closeNav();
 }, false);
 
@@ -20,4 +20,19 @@ function openNav() {
   document.body.classList.add('hamburgler-active');
 }
 
+//  Dropdown menu on navigation
+// Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
