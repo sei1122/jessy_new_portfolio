cfdd0c9bad3cf2295f423be003c82dbe4809a020
seiko igi <seikoigi@seikos-MBP.attlocal.net>
2020-08-23 13:58:02 -0700

    hide show and wrokshop

--- submit.php
+++ submit.php
@@ -1,9 +1,9 @@
 <?PHP
 
-	$to = "seikoigi@gmail.com"; #set addres to send for to 
+	$to = "jessyj.loreal@gmail.com"; #set addres to send for to 
 	$subject = "Message from Portfolio"; #set the subject line 
-	$headers = "From: FormMailer@seikoigi.com". "\r\n" .			
-				"Reply-To: FormMailer@seikoigi.com" . "\r\n"; #set the from address 
+	$headers = "From: FormMailer@jessyj.loreal@gmail.com". "\r\n" .			
+				"Reply-To: FormMailer@jessyj.loreal@gmail.com" . "\r\n"; #set the from address 
 	$forward = 1; # redirect? 1 : yes || 0 : no 
 	$location = "thanks.html"; #set page to redirect to, if 1 is above 
 
@@ -11,12 +11,12 @@
 	$date = date("Y/m/d"); 
 	$time = date("h:i:sa"); 
 
-	$msg = "Message from Portfolio site.  Submitted on $date at $time.\n\n"; 
+	$msg = "Message from Portfolio site. Submitted on $date at $time.\n\n"; 
 	     
 	foreach ($_POST as $key => $value) { 
 	    $msg .= ucfirst ($key) ." : ". $value . "\n"; 
 	} 
-	mail($to, $subject, $msg, $headers, " -fFormMailer@seikoigi.com"); 
+	mail($to, $subject, $msg, $headers, " -fFormMailer@jessyj.loreal@gmail.com"); 
 	if ($forward == 1) { 
 	    echo ('<script type="text/javascript">location.href = "' . $location . '"</script>');  
 	} 
